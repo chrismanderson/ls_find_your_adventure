dd879d0d3958481d3009d9ede7fa6c77beb0a716
Chris Anderson <christopher.anderson@gmail.com>
2012-06-28 18:32:38 -0400

    Removed quicksand

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery.ui.all
-//= require jquery.quicksand.js
 //= require twitter/bootstrap
 //= require chosen-jquery
 //= require underscore
