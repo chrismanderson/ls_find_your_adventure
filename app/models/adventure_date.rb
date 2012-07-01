e9a99d5f173b6feff44d163f26a67bb2e43099ae
Chris Anderson <christopher.anderson@gmail.com>
2012-07-01 15:27:30 -0400

    Available date view

--- app/models/adventure_date.rb
+++ app/models/adventure_date.rb
@@ -1,4 +1,8 @@
 class AdventureDate < ActiveRecord::Base
   attr_accessible :date
   has_many :adventure_date_items
+
+  def self.group_by_month dates
+   dates.group_by { |date| date.date.strftime("%B") }
+  end
 end
