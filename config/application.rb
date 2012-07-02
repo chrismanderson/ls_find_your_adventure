771700ca25808af08578776fd9608f3f49b0f8fb
Chris Anderson <christopher.anderson@gmail.com>
2012-07-02 09:24:11 -0400

    fix asset bug

--- config/application.rb
+++ config/application.rb
@@ -54,7 +54,7 @@ module FindYourAdventure
     config.active_record.whitelist_attributes = true
 
     # Enable the asset pipeline
-    config.assets.enabled = true
+    config.assets.initialize_on_precompile = false
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
