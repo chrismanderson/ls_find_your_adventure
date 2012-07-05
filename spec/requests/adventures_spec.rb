548d8b8f7fadda658f1edb745e52776f65ebecb9
Chris Anderson <christopher.anderson@gmail.com>
2012-07-05 10:12:10 -0400

    Stubbed out geocoding for request specs

--- spec/requests/adventures_spec.rb
+++ spec/requests/adventures_spec.rb
@@ -1,6 +1,13 @@
 require 'spec_helper'
 
+include Geocoding
+
 describe Adventure do
+
+  before do
+    Geocoding.stub_geocoding
+  end
+
   before(:each) do
     5.times do
       FactoryGirl.create(:adventure)
@@ -8,10 +15,12 @@ describe Adventure do
     end
   end
 
+  
+
   let!(:site_domain) { "http://example.com" }
   it "loads the page" do 
     visit "http://example.com"
-    page.should have_content "City?"
+    page.should have_content "City"
     page.should have_content Adventure.first.title
     page.should have_content Adventure.first.price
     page.should have_content Adventure.first.market.city
